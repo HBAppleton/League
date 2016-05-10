20e89df480a17e84e62c7df35813f3245fcc9d31
Heather Appleton <heathera@datacom.co.nz>
2016-05-10 14:04:11 +1200

    Added vDraw

---
+++ League.Repository/VDrawRepository.cs
@@ -0,0 +1,53 @@
+﻿using League.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace League.Repository
+{
+    public class VDrawRepository : GenericRepository<Vdraw>, IVDrawRepository
+    {
+        public VDrawRepository(MyDbContext context) : base(context)
+        {
+        }
+
+        public override IEnumerable<Vdraw> GetAll()
+        {
+            return _entities.Set<Vdraw>().AsEnumerable();
+        }
+
+        public Vdraw GetById(long Id)
+        {
+            return FindBy(n => n.DrawId == Id).FirstOrDefault();
+        }
+
+
+        public IEnumerable<Vdraw> GetBySeason(int Season)
+        {
+            return _entities.Set<Vdraw>().Where(n => n.Season == Season).AsEnumerable();
+        }
+
+        public IEnumerable<Vdraw> GetByWeekId(long WeekId)
+        {
+            return _entities.Set<Vdraw>().Where(n => n.WeekId == WeekId).AsEnumerable();
+        }
+
+        public IEnumerable<Vdraw> GetByTeamId(long TeamId)
+        {
+            return _entities.Set<Vdraw>().Where(n => n.TeamId == TeamId).AsEnumerable();
+        }
+
+        public IEnumerable<Vdraw> GetByBowlerId(long BowlerId)
+        {
+            return _entities.Set<Vdraw>().Where(n => n.BowlerId == BowlerId).AsEnumerable();
+        }
+
+        public IEnumerable<Vdraw> GetByScoresheetId(long ScoresheetId)
+        {
+            return _entities.Set<Vdraw>().Where(n => n.ScoresheetId == ScoresheetId).AsEnumerable();
+        }
+
+    }
+}
---
+++ League.Service/Services/VDrawService.cs
@@ -0,0 +1,54 @@
+﻿using League.Model;
+using League.Repository;
+using League.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace League.Service.Services
+{
+    public class VDrawService : EntityService<Vdraw>, IVDrawService
+    {
+        IUnitOfWork _unitOfWork;
+        IVDrawRepository _vDrawRepository;
+
+        public VDrawService(IUnitOfWork unitOfWork, IVDrawRepository VDrawRepository) : base(unitOfWork, VDrawRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _vDrawRepository = VDrawRepository;
+        }
+
+        public Vdraw GetById(long Id)
+        {
+            return _vDrawRepository.GetById(Id);
+        }
+/*
+        public Vdraw GetBySeason(int Season)
+        {
+            return _vDrawRepository.GetBySeason(Season);
+        }
+
+        public Vdraw GetByWeekId(long WeekId)
+        {
+            return _vDrawRepository.GetByWeekId(WeekId);
+        }
+
+        public Vdraw GetByTeamId(long TeamId)
+        {
+            return _vDrawRepository.GetByTeamId(TeamId);
+        }
+
+        public Vdraw GetByBowlerId(long BowlerId)
+        {
+            return _vDrawRepository.GetByBowlerId(BowlerId);
+        }
+
+        public Vdraw GetByScoresheetId(long ScoresheetId)
+        {
+            return _vDrawRepository.GetByScoresheetId(ScoresheetId);
+        }
+*/
+    }
+}
